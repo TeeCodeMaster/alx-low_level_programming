059bb0f18933de7fce7519ddd6509d5258ebf78b
Tsetsana Jobo Khobotlo <tsetsankhobotlo@gmail.com>
2023-10-23 14:18:43 -0700

    a file that contains the password for the crackme2 executable

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include *main.h*
+/**
+ * set strin - sets the value of a pointer to  char
+ * @s - pointer to pointer
+ * @to - pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
